122eeed7e44b99a0d33b742d5278572e9ec2bd86
Laura yang <yangziya@msu.edu>
2020-02-19 22:34:56 -0500

    created the project; twp buttons; half way through trans to the rule page

---
+++ app/src/main/java/edu/msu/yangziya/project1/Rules.java
@@ -0,0 +1,14 @@
+package edu.msu.yangziya.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rules extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
