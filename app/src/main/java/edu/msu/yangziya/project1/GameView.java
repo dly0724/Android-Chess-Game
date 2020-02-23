1e909e4616cf50c3632fd1531d01058f22feb784
lynn <lingyundaiseven@gmail.com>
2020-02-22 20:12:08 -0500

    Set the game area after clicking start game

---
+++ app/src/main/java/edu/msu/yangziya/project1/GameView.java
@@ -0,0 +1,50 @@
+package edu.msu.yangziya.project1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+
+    /**
+     * The actual game
+     */
+    private Game game;
+
+    /**
+     * Paint object we will use to draw a line
+     */
+    private Paint linePaint;
+
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        game = new Game(getContext());
+
+        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setColor(0xff008000);
+        linePaint.setStrokeWidth(3);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        game.draw(canvas);
+    }
+}
--- app/src/main/java/edu/msu/yangziya/project1/MainActivity.java
+++ app/src/main/java/edu/msu/yangziya/project1/MainActivity.java
@@ -1,7 +1,6 @@
 package edu.msu.yangziya.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
