2acb54a647e021a0cd833b3367851be4658d3986
Sam Vitu <vitusamu@msu.edu>
2020-02-24 20:13:28 -0500

    Drew pieces on board

---
+++ app/src/main/java/edu/msu/yangziya/project1/ChessPiece.java
@@ -0,0 +1,165 @@
+package edu.msu.yangziya.project1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+import java.util.Random;
+
+/**
+ * This class represents one piece of our chess game.
+ */
+public class ChessPiece {
+    /**
+     * The image for the actual piece.
+     */
+    private Bitmap piece;
+
+    /**
+     * x location.
+     * We use relative x locations in the range 0-1 for the center
+     * of the chess piece.
+     */
+    private float x;
+
+    /**
+     * y location
+     */
+    private float y;
+
+    /**
+     * The chess piece ID
+     */
+    private int id;
+
+//    /**
+//     * We consider a piece to be in the right location if within
+//     * this distance.
+//     */
+//    final static float SNAP_DISTANCE = 0.05f;
+
+    public ChessPiece(Context context, int id, float x, float y) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+
+        piece = BitmapFactory.decodeResource(context.getResources(), id);
+    }
+
+    /**
+     * Draw the chess piece
+     * @param canvas Canvas we are drawing on
+     * @param marginX Margin x value in pixels
+     * @param marginY Margin y value in pixels
+     * @param boardSize Size we draw the board in pixels
+     * @param scaleFactor Amount we scale the chess pieces when we draw them
+     */
+    public void draw(Canvas canvas, int marginX, int marginY, int boardSize, float scaleFactor) {
+        canvas.save();
+
+        // Convert x,y to pixels and add the margin, then draw
+        canvas.translate(marginX + x * boardSize, marginY + y * boardSize);
+
+        // Scale it to the right size
+        canvas.scale(scaleFactor, scaleFactor);
+
+        // This magic code makes the center of the piece at 0, 0
+        canvas.translate(-piece.getWidth() / 2f, -piece.getHeight() / 2f);
+
+        // Draw the bitmap
+        canvas.drawBitmap(piece, 0, 0, null);
+        canvas.restore();
+    }
+
+    /**
+     * Test to see if we have touched a chess piece
+     * @param testX X location as a normalized coordinate (0 to 1)
+     * @param testY Y location as a normalized coordinate (0 to 1)
+     * @param boardSize the size of the board in pixels
+     * @param scaleFactor the amount to scale a piece by
+     * @return true if we hit the piece
+     */
+    public boolean hit(float testX, float testY,
+                       int boardSize, float scaleFactor) {
+
+        // Make relative to the location and size to the piece size
+        int pX = (int)((testX - x) * boardSize / scaleFactor) +
+                piece.getWidth() / 2;
+        int pY = (int)((testY - y) * boardSize / scaleFactor) +
+                piece.getHeight() / 2;
+
+        if(pX < 0 || pX >= piece.getWidth() ||
+                pY < 0 || pY >= piece.getHeight()) {
+            return false;
+        }
+
+        // We are within the rectangle of the piece.
+        // Are we touching actual picture?
+        return (piece.getPixel(pX, pY) & 0xff000000) != 0;
+    }
+
+    /**
+     * Move the chess piece by dx, dy
+     * @param dx x amount to move
+     * @param dy y amount to move
+     */
+    public void move(float dx, float dy) {
+        x += dx;
+        y += dy;
+    }
+
+//    /**
+//     * If we are within SNAP_DISTANCE of the correct
+//     * answer, snap to the correct answer exactly.
+//     * @return true if within snap distance, else false
+//     */
+//    public boolean maybeSnap() {
+////        if(Math.abs(x - finalX) < SNAP_DISTANCE &&
+////                Math.abs(y - finalY) < SNAP_DISTANCE) {
+////
+////            x = finalX;
+////            y = finalY;
+////            return true;
+////        }
+//
+//        return false;
+//    }
+
+//    /**
+//     * Determine if this piece is snapped in place
+//     * @return true if snapped into place
+//     */
+//    public boolean isSnapped() {
+//        return x == finalX && y == finalY;
+//    }
+//
+//    /**
+//     * Shuffle the location of this piece
+//     * @param rand A random number generator
+//     */
+//    public void shuffle(Random rand) {
+//        x = rand.nextFloat();
+//        y = rand.nextFloat();
+//    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- app/src/main/java/edu/msu/yangziya/project1/Game.java
+++ app/src/main/java/edu/msu/yangziya/project1/Game.java
@@ -1,8 +1,16 @@
 package edu.msu.yangziya.project1;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+
 
 public class Game {
 
@@ -22,8 +30,28 @@ public class Game {
      */
     private Paint outlinePaint;
 
+    /**
+     * Completed game bitmap
+     */
+    private Bitmap gameComplete;
+
 
     private Paint paint1;
+
+    /**
+     * The game board image
+     */
+    private Bitmap boardImage;
+
+    /**
+     * How much we scale the puzzle pieces
+     */
+    private float scaleFactor;
+
+    /**
+     * Collection of chess pieces
+     */
+    private ArrayList<ChessPiece> pieces = new ArrayList<>();
 
 
     public Game(Context context) {
@@ -39,6 +67,72 @@ public class Game {
         paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint1.setColor(0xff00ff00);
 
+        // Load the solved puzzle image
+        gameComplete = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_background);
+//        Drawable drawable = ContextCompat(context, R.drawable.ic_launcher_background);
+//        gameComplete = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+//                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        Resources res = context.getResources();
+        boardImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_background);
+//        board_image = BitmapFactory.decodeResource(res, R.drawable.ic_launcher_background);
+
+        //
+        // Load the Black pieces
+        //
+
+        // Rooks
+        pieces.add(new ChessPiece(context, R.drawable.chess_rdt45, 0.059f, 0.1f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_rdt45, 0.939f, 0.1f));
+        // Knights
+        pieces.add(new ChessPiece(context, R.drawable.chess_ndt45, 0.179f, 0.1f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_ndt45, 0.819f, 0.1f));
+        // Bishops
+        pieces.add(new ChessPiece(context, R.drawable.chess_bdt45, 0.319f, 0.1f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_bdt45, 0.689f, 0.1f));
+        // Queen
+        pieces.add(new ChessPiece(context, R.drawable.chess_qdt45, 0.439f, 0.1f));
+        // King
+        pieces.add(new ChessPiece(context, R.drawable.chess_kdt45, 0.569f, 0.1f));
+
+        // Pawns
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.059f, 0.238f));  // Leftmost
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.179f, 0.238f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.319f, 0.238f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.439f, 0.238f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.569f, 0.238f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.689f, 0.238f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.819f, 0.238f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_pdt45, 0.939f, 0.238f)); // Rightmost
+
+
+        //
+        // Load the White pieces
+        //
+
+        // Rooks
+        pieces.add(new ChessPiece(context, R.drawable.chess_rlt45, 0.059f, 0.9f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_rlt45, 0.939f, 0.9f));
+        // Knights
+        pieces.add(new ChessPiece(context, R.drawable.chess_nlt45, 0.179f, 0.9f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_nlt45, 0.819f, 0.9f));
+        // Bishops
+        pieces.add(new ChessPiece(context, R.drawable.chess_blt45, 0.319f, 0.9f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_blt45, 0.689f, 0.9f));
+        // Queen
+        pieces.add(new ChessPiece(context, R.drawable.chess_qlt45, 0.439f, 0.9f));
+        // King
+        pieces.add(new ChessPiece(context, R.drawable.chess_klt45, 0.569f, 0.9f));
+
+        // Pawns
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.059f, 0.75f)); // Leftmost
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.179f, 0.75f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.319f, 0.75f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.439f, 0.75f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.569f, 0.75f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.689f, 0.75f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.819f, 0.75f));
+        pieces.add(new ChessPiece(context, R.drawable.chess_plt45, 0.939f, 0.75f));  // Rightmost
     }
 
     public void draw(Canvas canvas) {
@@ -55,23 +149,30 @@ public class Game {
         int marginX = (wid - gameSize) / 2;
         int marginY = (hit - gameSize) / 2;
 
-        //
+
         // Draw the border of the game area
-        //
         canvas.drawRect(marginX - 4, marginY - 4,
                 marginX + gameSize + 4, marginY + gameSize + 4, outlinePaint);
 
-        //
-        // Draw the game checkerboard
-        //
-        //canvas.drawRect(marginX / 8, marginY / 8, (marginX + gameSize) / 8,
-                //(marginY + gameSize) / 8, paint1);
 
-        //
+//        scaleFactor = (float)gameSize / (float)gameComplete.getWidth();
+        scaleFactor = (float)gameSize / (float)3824;
+//        // Draw the game checkerboard
+//
+//        canvas.drawRect(marginX / 8, marginY / 8, (marginX + gameSize) / 8,
+//                (marginY + gameSize) / 8, paint1);
+
+
         // Draw the outline of the game area
-        //
+
         canvas.drawRect(marginX, marginY, marginX + gameSize,
                 marginY + gameSize, fillPaint);
-        
+
+        // Draw the board image
+//        canvas.drawBitmap(boardImage, 0, 0, fillPaint);
+        for(ChessPiece piece : pieces) {
+            piece.draw(canvas, marginX, marginY, gameSize, scaleFactor);
+        }
+
     }
 }
