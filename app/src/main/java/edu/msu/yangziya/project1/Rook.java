69f3d6ce69b85b3c2ee7e4524d56b143b45d8358
Laura yang <yangziya@msu.edu>
2020-03-21 19:29:39 -0400

    win condition for Queen and Rook finished

--- app/src/main/java/edu/msu/yangziya/project1/Rook.java
+++ app/src/main/java/edu/msu/yangziya/project1/Rook.java
@@ -54,6 +54,95 @@ public class Rook extends ChessPiece {
     }
 
     protected void toKing(List<List<ChessPiece>> currentBoard){
+        if (this.color=='w'){
+            boolean up = true;
+            boolean down = true;
+            boolean right = true;
+            boolean left = true;
 
+            for (int i=1; i<8;i++){
+                if (up && rowIndex-i>=0 && currentBoard.get(rowIndex-i).get(columnIndex) != null){
+                    if (currentBoard.get(rowIndex-i).get(columnIndex).id == 4){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        up = false;
+                    }
+                }
+                if (right && columnIndex+i<8 && currentBoard.get(rowIndex).get(columnIndex+i) != null){
+                    if (currentBoard.get(rowIndex).get(columnIndex+i).id == 4){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        right = false;
+                    }
+                }
+                if (down && rowIndex+i<8 && currentBoard.get(rowIndex+i).get(columnIndex) != null){
+                    if (currentBoard.get(rowIndex+i).get(columnIndex).id == 4){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        down = false;
+                    }
+                }
+                if (left && columnIndex-i>=0 && currentBoard.get(rowIndex).get(columnIndex-i) != null){
+                    if (currentBoard.get(rowIndex).get(columnIndex-i).id == 4){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        left = false;
+                    }
+                }
+            }
+        }
+        if (this.color=='b'){
+            boolean up = true;
+            boolean down = true;
+            boolean right = true;
+            boolean left = true;
+
+            for (int i=1; i<8;i++){
+                if (up && rowIndex-i>=0 && currentBoard.get(rowIndex-i).get(columnIndex) != null){
+                    if (currentBoard.get(rowIndex-i).get(columnIndex).id == 28){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        up = false;
+                    }
+                }
+                if (right && columnIndex+i<8 && currentBoard.get(rowIndex).get(columnIndex+i) != null){
+                    if (currentBoard.get(rowIndex).get(columnIndex+i).id == 28){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        right = false;
+                    }
+                }
+                if (down && rowIndex+i<8 && currentBoard.get(rowIndex+i).get(columnIndex) != null){
+                    if (currentBoard.get(rowIndex+i).get(columnIndex).id == 28){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        down = false;
+                    }
+                }
+                if (left && columnIndex-i>=0 && currentBoard.get(rowIndex).get(columnIndex-i) != null){
+                    if (currentBoard.get(rowIndex).get(columnIndex-i).id == 28){
+                        kingCaptured=true;
+                        return;
+                    }
+                    else{
+                        left = false;
+                    }
+                }
+            }
+        }
     }
 }
